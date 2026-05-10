a887331214c0e6bf7c55f1b8a36a68bdd058dd7b
Mateo Paez <bpaezj@unal.edu.co>
2026-05-10 18:17:27 -0500

    Crear OdontogateApplication.java
    clase main para el springboot cuando corre en el sh

---
+++ src/main/java/com/odontogate/OdontogateApplication.java
@@ -0,0 +1,11 @@
+package com.odontogate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdontogateApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OdontogateApplication.class, args);
+    }
+}
