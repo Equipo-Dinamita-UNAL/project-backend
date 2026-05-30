485868f84047a09f954010a42d9447092261217a
Ashlee Tatiana Martínez Garzón <asmartinezga@unal.edu.co>
2026-05-30 15:53:49 -0500

    Modelos de las clases appointment y Schedule

---
+++ OdontoGate/src/main/java/com/OdontoGate/ArtefactoOdontoGate/model/Schedule.java
@@ -0,0 +1,31 @@
+package com.OdontoGate.OdontoGate.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalTime;
+
+@Data
+@Entity
+@Table(name = "schedule")
+public class Schedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "doctor_id")
+    private Doctor doctor;
+
+    @Column(name = "weekday")
+    private String weekday;
+
+    @Column(name = "start_time")
+    private LocalTime startTime;
+
+    @Column(name = "end_time")
+    private LocalTime endTime;
+
+    @Column(name = "is_available")
+    private Boolean isAvailable;
+}
