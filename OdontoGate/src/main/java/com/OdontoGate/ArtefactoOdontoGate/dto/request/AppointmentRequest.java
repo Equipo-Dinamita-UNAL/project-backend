8189280a2c2f04fc1614d833b835d72a0bdf565c
Ashlee Martínez <asmartinezga@unal.edu.co>
2026-06-08 01:11:00 -0500

    Revert "Limpieza del repositorio"
    This reverts commit f56630e05b478833dbd759dbe0bfd9323bbdc618.
    git push

---
+++ OdontoGate/src/main/java/com/OdontoGate/ArtefactoOdontoGate/dto/request/AppointmentRequest.java
@@ -0,0 +1,18 @@
+package com.OdontoGate.OdontoGate.dto.request;
+
+import lombok.Data;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+public class AppointmentRequest {
+
+    private Integer patientId;
+    private Integer doctorId;
+    private LocalDate date;
+    private LocalTime time;
+    private Integer doctorScheduleId;
+    private String status;
+    private String reason;
+    private Integer modifiedBy;
+}
